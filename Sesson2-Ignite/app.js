86f69beb0ecd762674a8a213fb0cd2a810fb21d6
Kshitij Singh <kshtjkmr10@gmail.com>
2024-12-26 17:32:12 +0530

    Ignite: Setup node react and parcel

---
+++ Sesson2-Ignite/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading1 = React.createElement("h1",{id:"heading1"},"Namaste React")
+const heading2 = React.createElement("h2",{id:"heading2"},"How are you doing?")
+const container = React.createElement("div",{id:"container"},[heading1,heading2])
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(container)
+
