5e1f586873a5fa86f8859b322a96d585a413e069
Kshitij Singh <kshtjkmr10@gmail.com>
2024-12-29 02:07:26 +0530

    toggle dummy login logout button

--- Sesson6-ExploringWorld/src/components/Header.js
+++ Sesson6-ExploringWorld/src/components/Header.js
@@ -1,15 +1,23 @@
+import {useState} from "react"
 import Title from "./Title"; 
-const Header = () =>(
-    <div className="header">
-        <Title/>
-        <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-                <li>Cart</li>
-            </ul>
+const Header = () =>{
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    return (
+        <div className="header">
+            <Title/>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+            { isLoggedIn?
+                <button onClick={()=>setIsLoggedIn(false)}>Logout</button>:
+                <button onClick={()=>setIsLoggedIn(true)}>Login</button>
+            }
         </div>
-    </div>
-)
+    )
+}
 export default Header;
